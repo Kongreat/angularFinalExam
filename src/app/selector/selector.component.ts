348f86a14259af3d7d5769752041c68ba5fa4edb
Konstantin Borodkin <kongreat13@gmail.com>
2020-11-26 15:40:11 +0600

    added main components, date, and part of styles

---
+++ src/app/selector/selector.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import {DateService} from '../shared/date.service';
+
+@Component({
+  selector: 'app-selector',
+  templateUrl: './selector.component.html',
+  styleUrls: ['./selector.component.scss']
+})
+export class SelectorComponent{
+
+  constructor(private dateService: DateService) { }
+
+
+  getDate(): any{
+    return this.dateService.date;
+  }
+
+}
